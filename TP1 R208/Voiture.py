936ef4e1ca719711fcaa93eaf56405f8fa9167fc
LerayValentin <148783160+LerayValentin@users.noreply.github.com>
2024-02-14 16:56:17 +0100

    Add files via upload

---
+++ TP1 R208/Voiture.py
@@ -0,0 +1,171 @@
+class Voiture1:
+    def __init__(self):
+        self.__marque = "féfé"
+        self.__modele = "F40"
+        self.__puiss = "beaucoup"
+        self.__couleur = "rouge"
+        self.__options = []
+
+    def __str__(self):
+        return f"Voici les caractéristiques de cette voiture:\n- Marque: {self.__marque}\n- Modele: {self.__modele}\n- Couleur: {self.__couleur}\n- Puissance: {self.__puiss}\n- Option(s): {self.__options}"
+
+
+    def affichage(self):
+        return f"La voiture est un(e) {self.__marque} {self.__modele} de puissance {self.__puiss} et de couleur {self.__couleur}."
+
+#       Accesseurs
+    def get_marque(self):
+        return self.__marque
+    def get_modele(self):
+        return self.__modele
+    def get_puiss(self):
+        return self.__puiss
+    def get_couleur(self):
+        return self.__couleur
+    def get_options(self):
+        return self.__options
+
+#       Accesseurs en écriture
+    def set_marque(self, marque):
+        self.__marque = marque
+    def set_modele(self, modele):
+        self.__modele = modele
+    def set_puiss(self, puiss):
+        self.__puiss = puiss
+    def set_couleur(self, couleur):
+        self.__couleur = couleur
+
+    def ajouter_option(self, opts):
+        self.__options.append(opts)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Voiture2:
+    def __init__(self, marque, modele, puissance, couleur):
+        self.__marque = marque
+        self.__modele = modele
+        self.__puiss = puissance
+        self.__couleur = couleur
+
+    def affichage(self):
+        return f"La voiture est un(e) {self.__marque} {self.__modele} de puissance {self.__puiss} et de couleur {self.__couleur}."
+
+#       Accesseurs
+    def get_marque(self):
+        return self.__marque
+    def get_modele(self):
+        return self.__modele
+    def get_puiss(self):
+        return self.__puiss
+    def get_couleur(self):
+        return self.__couleur
+
+#       Accesseurs en écriture
+    def set_marque(self, marque):
+        self.__marque = marque
+
+    def set_modele(self, modele):
+        self.__modele = modele
+
+    def set_puiss(self, puiss):
+        self.__puiss = puiss
+
+    def set_couleur(self, couleur):
+        self.__couleur = couleur
+
+
+
+
+
+
+
+class Voiture3:
+    def __init__(self, marque="Peugeot", modele="407SW", puissance="140", couleur="noir"):
+        self.__marque = marque
+        self.__modele = modele
+        self.__puiss = puissance
+        self.__couleur = couleur
+
+    def affichage(self):
+        return f"La voiture est un(e) {self.__marque} {self.__modele} de puissance {self.__puiss} et de couleur {self.__couleur}."
+
+#       Accesseurs
+    def get_marque(self):
+        return self.__marque
+    def get_modele(self):
+        return self.__modele
+    def get_puiss(self):
+        return self.__puiss
+    def get_couleur(self):
+        return self.__couleur
+
+#       Accesseurs en écriture
+    def set_marque(self, marque):
+        self.__marque = marque
+
+    def set_modele(self, modele):
+        self.__modele = modele
+
+    def set_puiss(self, puiss):
+        self.__puiss = puiss
+
+    def set_couleur(self, couleur):
+        self.__couleur = couleur
+
+
+
+
+
+
+
+
+
+class Voiture4:
+    def __init__(self, marque, modele, puissance, couleur="jaune"):
+        self.__marque = marque
+        self.__modele = modele
+        self.__puiss = puissance
+        self.__couleur = couleur
+
+    def affichage(self):
+        return f"La voiture est un(e) {self.__marque} {self.__modele} de puissance {self.__puiss} et de couleur {self.__couleur}."
+
+#       Accesseurs
+    def get_marque(self):
+        return self.__marque
+    def get_modele(self):
+        return self.__modele
+    def get_puiss(self):
+        return self.__puiss
+    def get_couleur(self):
+        return self.__couleur
+
+#       Accesseurs en écriture
+    def set_marque(self, marque):
+        self.__marque = marque
+
+    def set_modele(self, modele):
+        self.__modele = modele
+
+    def set_puiss(self, puiss):
+        self.__puiss = puiss
+
+    def set_couleur(self, couleur):
+        self.__couleur = couleur
---
+++ TP1 R208/main.py
@@ -0,0 +1,19 @@
+from Voiture import *
+
+mycar1 = Voiture1()
+mycar2 = Voiture2("BMW", "S2", "400", "noir")
+mycar3 = Voiture3()
+mycar4 = Voiture4("Chevrolet", "Camaro", "450")
+
+print(mycar1.affichage())
+print(mycar2.affichage())
+print(mycar3.affichage())
+print(mycar4.affichage())
+
+print(mycar1.get_marque())
+mycar1.set_marque("BMW")
+print(mycar1.get_marque())
+
+print(mycar1)
+mycar1.ajouter_option("Pack M")
+print(mycar1)
