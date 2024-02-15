71875f3aba5ce63bdf0dd79713a81514aae6e2cf
LerayValentin <148783160+LerayValentin@users.noreply.github.com>
2024-02-15 09:56:20 +0100

    Delete TP1 R208 directory

--- TP1 R208/Citron.py
+++
@@ -1,9 +0,0 @@
-class Citron:
-    def __init__(self):
-        self.couleur = "jaune"
-
-    def affichage(self):
-        return f"La couleur est {self.couleur}"
-
-citron1 = Citron()
-print(citron1)
