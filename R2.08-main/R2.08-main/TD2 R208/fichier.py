a02b4bee7f8c72ab6a91cbb354d3535d08ffc152
LerayValentin <148783160+LerayValentin@users.noreply.github.com>
2024-03-23 11:32:25 +0100

    Delete R2.08-main/R2.08-main directory

--- R2.08-main/R2.08-main/TD2 R208/fichier.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    fichier = open("txt.txt", "r")
-
-    for lines in fichier:
-        fichier.close()
-        fichier = open("txt.txt", "w")
-        lines = lines.rstrip("\r\n")
-        print(lines)
--- R2.08-main/R2.08-main/TP3 R208/main.py
+++
@@ -1,8 +0,0 @@
-from TP3 import *
-
-film1 = Film("Nom du film", "10-10-2020", 4, "listeee")
-
-film1.title
-film1.date
-film1.note
-film1.liste_avis
